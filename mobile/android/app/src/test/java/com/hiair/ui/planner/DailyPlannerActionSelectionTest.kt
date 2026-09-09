7673e76ec0f9637478954b0fe8a8ae8a44d6b86a
Aleksandr Potkin <2qjckdknjf@privaterelay.appleid.com>
2026-09-09 03:10:27 +0200

    fix(android): use project junit test imports

--- mobile/android/app/src/test/java/com/hiair/ui/planner/DailyPlannerActionSelectionTest.kt
+++ mobile/android/app/src/test/java/com/hiair/ui/planner/DailyPlannerActionSelectionTest.kt
@@ -1,7 +1,7 @@
 package com.hiair.ui.planner
 
-import kotlin.test.Test
-import kotlin.test.assertEquals
+import org.junit.Assert.assertEquals
+import org.junit.Test
 
 class DailyPlannerActionSelectionTest {
     @Test
